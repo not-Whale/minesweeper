354e0329d44c5a85b72d5e74ca7a0ff757c89941
not-Whale <rezepinn@yandex.ru>
2022-11-09 04:02:59 +0300

    Добавил ошибки состояний

---
+++ minesweeper/errors.go
@@ -0,0 +1,41 @@
+package minesweeper
+
+import (
+	"fmt"
+)
+
+type errCellIsMarked struct {
+	x, y int
+}
+
+type errCellIsNotMarked struct {
+	x, y int
+}
+
+type errGameOver struct {
+	x, y int
+}
+
+type errYouWin struct{}
+
+type errUnknownLevel int
+
+func (e errCellIsMarked) Error() string {
+	return fmt.Sprintf("ЯЧЕЙКА [%v, %v] УЖЕ ОТМЕЧЕНА", e.x, e.y)
+}
+
+func (e errCellIsNotMarked) Error() string {
+	return fmt.Sprintf("ЯЧЕЙКА [%v, %v] И ТАК НЕ ОТМЕЧЕНА", e.x, e.y)
+}
+
+func (e errGameOver) Error() string {
+	return fmt.Sprintf("ВЫ ПРОИГРАЛИ: В ЯЧЕЙКЕ [%v, %v] ОКАЗАЛАСЬ БОМБА", e.x, e.y)
+}
+
+func (e errUnknownLevel) Error() string {
+	return fmt.Sprintf("НЕИЗВЕСТНЫЙ УРОВЕНЬ СЛОЖНОСТИ %v", int(e))
+}
+
+func (e errYouWin) Error() string {
+	return "ВЫ ПОБЕДИЛИ"
+}
