62ee36c77a8fb39add5e03df934f37eea99d2778
not-Whale <rezepinn@yandex.ru>
2022-11-09 04:25:12 +0300

    Изменил текст ошибок

--- minesweeper/errors.go
+++ minesweeper/errors.go
@@ -25,25 +25,25 @@ type errYouWin struct{}
 type errUnknownLevel int
 
 func (e errCellIsAlreadyMarked) Error() string {
-	return fmt.Sprintf("ЯЧЕЙКА [%v, %v] УЖЕ ОТМЕЧЕНА", e.x, e.y)
+	return fmt.Sprintf("Ячейка [%v, %v] уже отмечена!", e.x, e.y)
 }
 
 func (e errCellIsAlreadyNotMarked) Error() string {
-	return fmt.Sprintf("ЯЧЕЙКА [%v, %v] И ТАК НЕ ОТМЕЧЕНА", e.x, e.y)
+	return fmt.Sprintf("Ячейка [%v, %v] и так не отмечена!", e.x, e.y)
 }
 
 func (e errGameOver) Error() string {
-	return fmt.Sprintf("ВЫ ПРОИГРАЛИ: В ЯЧЕЙКЕ [%v, %v] ОКАЗАЛАСЬ БОМБА", e.x, e.y)
+	return fmt.Sprintf("Вы проиграли! В ячейке [%v, %v] оказалась бомба!", e.x, e.y)
 }
 
 func (e errOutOfRange) Error() string {
-	return fmt.Sprintf("ЯЧЕЙКА [%v, %v] НАХОДИТСЯ ЗА ПРЕДЕЛЯМИ ИГРОВОГО ПОЛЯ", e.x, e.y)
+	return fmt.Sprintf("Ячейка [%v, %v] находится за пределами игрового поля!", e.x, e.y)
 }
 
 func (e errUnknownLevel) Error() string {
-	return fmt.Sprintf("НЕИЗВЕСТНЫЙ УРОВЕНЬ СЛОЖНОСТИ %v", int(e))
+	return fmt.Sprintf("Неизвестный уровень сложности %v!", int(e))
 }
 
 func (e errYouWin) Error() string {
-	return "ВЫ ПОБЕДИЛИ"
+	return "Поздравляю! Вы победили!"
 }
